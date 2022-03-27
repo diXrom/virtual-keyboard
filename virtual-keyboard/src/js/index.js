86bcddfe73113aee0fdeb3c7355ed9f00a46cbd7
Tolegen Timur <demigodkz@gmail.com>
2022-03-27 22:01:48 +0600

    feat: add class ElemFooter

---
+++ virtual-keyboard/src/js/index.js
@@ -0,0 +1,7 @@
+import Header from './components/ElemHeader.js';
+import Main from './components/ElemMain.js';
+import Footer from './components/ElemFooter.js';
+const header = new Header().render();
+const main = new Main().render()
+const footer = new Footer().render()
+document.body.append(header, main, footer);
